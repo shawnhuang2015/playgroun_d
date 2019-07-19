cf146dfd94a6f658ab7f2a7313e95e44b215b38f
Shawn Huang <shawn@aisense.com>
2019-07-18 23:56:41 -0700

    Play with tre

--- tree/suffix_tree.js
+++ tree/suffix_tree.js
@@ -1,38 +1,59 @@
 (() => {
   let s1 = 'banana';
-
+  let s2 = 'abcabxabcd';
   class SuffixTree {
     constructor(str) {
       this.root = new TreeNode();
       this.size = str.length;
 
       for (let i = 0; i < this.size + 1; ++i) {
-        this.add(new TreeNode(str.slice(i)));
+        this.add(new TreeNode(str.slice(i), i));
       }
     }
 
     add(node) {
       this.root.add(node);
     }
+
+    search(str) {
+      return this.root.search(new TreeNode(str)); // .map((index) => this.size - index);
+    }
   }
 
   class TreeNode {
-    constructor(str) {
+    constructor(str, index) {
       this.children = [];
       this.value = str || '';
       this.parent = null;
-      this.index = this.value.length;
+      this.index = index;
+
+      this.indices = [];
+    }
+
+    get isEnd() {
+      return this.children.length === 0;
     }
 
     get size() {
       return this.value.length;
     }
 
+    toRoot() {
+      let node = this;
+      let result = '';
+      while (node) {
+        result = node.value + result;
+        node = node.parent;
+      }
+
+      return result;
+    }
+
     split(len) {
-      let newNode = new TreeNode(this.value.slice(len));
+      let newNode = new TreeNode(this.value.slice(len), this.index);
       this.value = this.value.slice(0, len);
 
-      newNode.index = this.index;
+      this.indices.push(this.index);
       this.index = 0;
 
       newNode.children = this.children;
@@ -42,6 +63,24 @@
 
       this.children = [newNode];
       newNode.parent = this;
+    }
+
+    search(node) {
+      for (let i = 0; i < this.children.length; ++i) {
+        const child = this.children[i];
+        let l = child.checkMatched(node);
+
+        if (l > 0) {
+          if (l != node.size) {
+            node.value = node.value.slice(1);
+            return child.search(node);
+          } else {
+            return child.getIndex();
+          }
+        }
+      }
+
+      return [];
     }
 
     add(node) {
@@ -77,12 +116,31 @@
 
       return i;
     }
+
+    getIndex() {
+      let queue = [this];
+      let result = [];
+
+      while (queue.length > 0) {
+        let node = queue.pop();
+
+        if (node.isEnd) {
+          result.push(node.index);
+        } else {
+          queue.push(...node.children);
+        }
+      }
+
+      return result;
+    }
   }
 
-  let tree = new SuffixTree(s1);
+  // let tree = new SuffixTree(s1);
+  let tree2 = new SuffixTree(s2);
 
+  console.log(tree2.search('ab'));
   // dfs(tree.root);
-  bfs(tree.root);
+  // bfs(tree.root);
 
   function dfs(node) {
     for (let i = 0; i < node.children.length; ++i) {
